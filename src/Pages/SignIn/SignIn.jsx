e4feeff39b9b3c6fd3ae4c13ed97da3487599a9a
akauf13 <adammkaufman10@gmail.com>
2023-03-14 14:19:24 -0400

    login with google button

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -3,26 +3,28 @@ import logo from "../../images/logo.png";
 import { Link, useNavigate } from "react-router-dom";
 import { auth, signInWithGoogle } from "../../services/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect } from "react";
 
 function SignIn() {
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
+  useEffect(() => {
+    if (loading) {
+      // loading screen
+      return;
+    }
+    if (user) navigate("/home");
+  }, [user, loading]);
   return (
-    <>
+    <div>
       <img src={logo} alt="logo" className="signin-logo" />
       <p>Sign In</p>
       <div className="sign-in-form-container">
-        <form className="sign-in-form">
-          <input type="text" name="name" placeholder="Your name" />
-          <input type="text" name="reply_to" placeholder="Your e-mail" />
-          <input type="text" name="password" placeholder="Your e-mail" />
-        </form>
+        {/* sign in using the google pop up */}
+        <button className="sign-in-button" onClick={signInWithGoogle}>Login With Google</button>
       </div>
-      <button className="sign-in-button">Log In</button>
-      <Link to="/sign-up">
-        <button className="sign-in-button">Create an account</button>
-      </Link>
-    </>
+
+    </div>
   );
 }
 
