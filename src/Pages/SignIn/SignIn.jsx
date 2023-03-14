54a159e08af56494f2ddb00f0d32b08fc3f03ef7
akauf13 <adammkaufman10@gmail.com>
2023-03-14 14:01:52 -0400

    removed browser router

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,7 +1,7 @@
 import "./SignIn.css";
 import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function SignIn() {
   return (
