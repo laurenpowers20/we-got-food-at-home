c683ee761e054d651cb99586bd2ed3cfa08ae66e
vvsuji <suji.falcon@generalassemb.ly>
2023-03-14 17:51:45 -0400

    started character function, installed sass

---
+++ src/Pages/Character/Character.jsx
@@ -0,0 +1,8 @@
+import { React, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+function Character() {
+	const [character, setCharacter] = useState({});
+}
+
+export default Character;
