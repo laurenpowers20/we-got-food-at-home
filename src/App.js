93e5331fa88dedee18e8783ce060ef8b01acbfc9
Lauren Powers <laurenpowers@Laurens-Air.lan>
2023-03-14 17:18:31 -0400

    profile page layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import SignIn from "./Pages/SignIn/SignIn";
 import AddIngredients from "./Pages/AddIngredients/AddIngredients";
+import Home from "./Pages/Home/Home";
+import Profile from "./Pages/Profile/Profile";
+import SetGoal from "./Pages/SetGoal/SetGoal";
+
+import SignIn from "./Pages/SignIn/SignIn";
 
 function App() {
   return (
@@ -11,6 +14,8 @@ function App() {
         <Route path="/" element={<SignIn />} />
         <Route path="/home" element={<Home />} />
         <Route path="/ingredients" element={<AddIngredients />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/set-goal" element={<SetGoal />} />
       </Routes>
     </div>
   );
--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,20 +1,25 @@
 import { IoMdTrophy } from "react-icons/io";
 import { IoTrophyOutline } from "react-icons/io5";
+import "./Profile.css";
 
 function Profile() {
   return (
     <div>
       <h1>Name</h1>
-      <div>Photo</div>
-      <div>level1</div>
-      <div>level2</div>
+      <div className="profile-top">
+        <div className="profile-photo">Photo</div>
+        <div className="profile-level container">
+        <div className="profile-level-bar one"></div>
+          <div className="profile-level-bar two"></div>
+          </div>
+      </div>
       <p>Your points: 40</p>
       <p>Your collection</p>
       <div>
         <IoMdTrophy /> <IoMdTrophy /> <IoMdTrophy /> <IoMdTrophy />{" "}
         <IoTrophyOutline /> <IoTrophyOutline /> <IoTrophyOutline />
       </div>
-      <button>Ready to cook?</button>
+      <button className="profile-button">Ready to cook?</button>
     </div>
   );
 }
