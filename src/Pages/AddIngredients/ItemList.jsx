aeb910ee10603de61a648d074ff97b75592a7b02
Lauren Powers <laurenpowers@Laurens-Air.lan>
2023-03-14 21:15:27 -0400

    ingredients styling

--- src/Pages/AddIngredients/ItemList.jsx
+++ src/Pages/AddIngredients/ItemList.jsx
@@ -3,23 +3,19 @@ import "./AddIngredients.css";
 
 const ItemList = ({ item, selectItem, deleteItem }) => {
   return (
-    <div>
-      <div className="ingredients-list-items">
-        <input
-          className="checkbox"
-          onChange={() => selectItem(item)}
-          type="checkbox"
-          checked={item.selected ? "checked" : ""}
-        />
-        <div>
-          <p onClick={() => selectItem(item)}>{item.text}</p>
-        </div>
-        <div>
-          <button className="x-button" onClick={() => deleteItem(item.id)}>
-            x
-          </button>
-        </div>
-      </div>
+    <div className="ingredients-list-items">
+      <input
+        className="checkbox"
+        onChange={() => selectItem(item)}
+        type="checkbox"
+        checked={item.selected ? "checked" : ""}
+      />
+      <li>
+        <p onClick={() => selectItem(item)}>{item.text}</p>
+      </li>
+      <button className="x-button" onClick={() => deleteItem(item.id)}>
+        x
+      </button>
     </div>
   );
 };
