8b1901e1bb417850cb58649a65aea7221857823c
Lauren Powers <laurenpowers@Laurens-Air.lan>
2023-03-15 10:29:38 -0400

    set goal stlying complete

--- src/Pages/SetGoal/SetGoal.jsx
+++ src/Pages/SetGoal/SetGoal.jsx
@@ -7,14 +7,16 @@ function SetGoal() {
     <div className="set-goal-component">
       <div className="set-goal-back-icon">
         <SlArrowLeft />
-        </div>
-        <h1>Set Goal</h1>
-    
+      </div>
+      <h1>Set Goal</h1>
+
       <p>I'd like to cook...</p>
       <div className="set-goal">
-        <div className="set-goal-divs">1-2 x week</div>
-        <div className="set-goal-divs">3-4 x week</div>
-        <div className="set-goal-divs">5-7 x week</div>
+        <div className="set-goal-divs">
+          1-2 x week<div className="set-goal-level">Easy</div>
+        </div>
+        <div className="set-goal-divs">3-4 x week<div className="set-goal-level intermediate">Intermediate</div></div>
+        <div className="set-goal-divs">5-7 x week<div className="set-goal-level">Hard</div></div>
         <div className="set-goal-divs">I'm not sure yet</div>
       </div>
     </div>
