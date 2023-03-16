ebbc3d43c45fc4974a7ce12976692d0db5d4950d
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2023-03-16 15:17:42 -0400

    clean branch

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { Outlet, Link } from "react-router-dom";
-import "./Layout.css";
+// import React from "react";
+// import { Outlet, Link } from "react-router-dom";
+// import "./Layout.css";
 
-function Layout() {
-  return (
-    <div>
-      <>
-        <Outlet />
-        <div className="nav-container">
-          <div className="nav-list-item">
-            <Link to="/home">Home</Link>{" "}
-          </div>
-          <div className="nav-list-item">
-            {" "}
-            <Link to="/ingredients">Ingredients</Link>{" "}
-          </div>
-        </div>
-      </>
-    </div>
-  );
-}
+// function Layout() {
+//   return (
+//     <div>
+//       <>
+//         <Outlet />
+//         <div className="nav-container">
+//           <div className="nav-list-item">
+//             <Link to="/home">Home</Link>{" "}
+//           </div>
+//           <div className="nav-list-item">
+//             {" "}
+//             <Link to="/ingredients">Ingredients</Link>{" "}
+//           </div>
+//         </div>
+//       </>
+//     </div>
+//   );
+// }
 
-export default Layout;
+// export default Layout;
