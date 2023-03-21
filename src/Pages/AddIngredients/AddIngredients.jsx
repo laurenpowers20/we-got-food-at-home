487d358de575a27d7f449135322a21adf5ff0acc
Lauren Powers <laurenpowers@Laurens-Air.lan>
2023-03-21 08:36:44 -0400

    fixed error that was preventing recipe from loading

--- src/Pages/AddIngredients/AddIngredients.jsx
+++ src/Pages/AddIngredients/AddIngredients.jsx
@@ -7,7 +7,7 @@ import Recipes from "../../Components/Recipes";
 import "../AddIngredients/AddIngredients.css";
 import { Hearts } from "react-loading-icons";
 import { IoChevronBackOutline } from "react-icons/io5";
-
+import { Configuration, OpenAIApi } from "openai";
 import {
   query,
   collection,
@@ -18,7 +18,6 @@ import {
   deleteDoc,
   where,
 } from "firebase/firestore";
-import { Configuration, OpenAIApi } from "openai";
 
 function AddIngredients() {
   const [prompttest, setPrompt] = useState("");
@@ -131,129 +130,105 @@ function AddIngredients() {
   };
 
   return (
-		<div>
-			<div>
-				{/* <Link to="/ingredients/recipes">Recipes</Link> */}
-				<Link to='/home'>
-					<IoChevronBackOutline
-						style={{
-							alignItems: 'center',
-							color: 'gray',
-							display: 'flex',
-							marginTop: '20px',
-							marginLeft: '10px',
-							fontSize: '40px',
-						}}
-					/>
-				</Link>{' '}
-			</div>
-			{recipe ? (
-				true
-			) : (
-				<div className='ingredients-div'>
-					<div id='ingredients-header'>
-						<h1>Enter Ingredients</h1>
-					</div>
-					<div className='ingredients-form'>
-						<form onSubmit={addItem}>
-							<input
-								className='custom-input'
-								value={input}
-								onChange={(e) => setInput(e.target.value)}
-								type='text'
-								placeholder='Add Item'
-							/>
-							<button className='add-btn'>Add</button>
-						</form>
-						<ul className='ingredients-div'>
-							{items.map((item, index) => (
-								<ItemList
-									key={index}
-									item={item}
-									selectItem={selectItem}
-									deleteItem={deleteItem}
-								/>
-							))}
-						</ul>
-						{selectedItems.length < 1 ? null : (
-							<p>{`You are including ${selectedItems.length} of the ${items.length} items in your recipe`}</p>
-						)}
-					</div>
-					<div>
-						<div>
-							{/* Submits the prompt with the selected items */}
-
-							<form onSubmit={handleSubmit}>
-								<button
-									className='home-create-recipe-button'
-									onClick={handleClick}
-									type='submit'>
-									Create a recipe
-								</button>
-							</form>
-						</div>
-					</div>
-
-					{load ? (
-						<Hearts
-							stroke='#f09133'
-							fill='#ed7f12'
-							strokeOpacity={0.1}
-							fillOpacity={1}
-							speed={0.75}
-						/>
-					) : (
-						' '
-					)}
-
-					{/* <div> */}
-					{/* <div> */}
-					{/* {recipe ? (
-            <div className="recipe">
-              <h1>{response.name}</h1>
- 
-              <div>
-                <h2>Ingredients</h2>
-                <ol>
-                  {response.ingredients.map((ingredient, index) => (
-                    <li key={index}>{ingredient}</li>
-                  ))}
-                </ol>
-              </div>
-              <div>
-                <h2>Instructions</h2>
-                <ol>
-                  {response.instructions.map((instruction, index) => (
-                    <li key={index}>{instruction}</li>
-                  ))}
-                </ol>
-              </div>
+    <>
+      <div>
+        <div>
+          
+          <Link to="/home">
+            <IoChevronBackOutline
+              style={{
+                alignItems: "center",
+                color: "gray",
+                display: "flex",
+                marginTop: "20px",
+                marginLeft: "10px",
+                fontSize: "40px",
+              }}
+            />
+          </Link>{" "}
+        </div>
+      </div>
+      {recipe ? (
+        true
+      ) : (
+        <div className="ingredients-div">
+          <div>
+            <div>
+              <h1>Enter Ingredients</h1>
+              <form onSubmit={addItem}>
+                <input
+                  className="custom-input"
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                  type="text"
+                  placeholder="Add Item"
+                />
+                <button className="add-btn">Add</button>
+              </form>
+              <ul className="ingredients-div">
+                {items.map((item, index) => (
+                  <ItemList
+                    key={index}
+                    item={item}
+                    selectItem={selectItem}
+                    deleteItem={deleteItem}
+                  />
+                ))}
+              </ul>
+              {selectedItems.length < 1 ? null : (
+                <p>{`You are including ${selectedItems.length} of the ${items.length} items in your recipe`}</p>
+              )}
             </div>
+          </div>
+          <div>
+            <div>
+              {/* Submits the prompt with the selected items */}
+
+              <form onSubmit={handleSubmit}>
+                <button
+                  className="home-logout-button"
+                  onClick={handleClick}
+                  type="submit"
+                >
+                  Create a recipe
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {load ? (
+        <Hearts
+          stroke="#f09133"
+          fill="#ed7f12"
+          strokeOpacity={0.1}
+          fillOpacity={1}
+          speed={0.75}
+        />
+      ) : (
+        " "
+      )}
+
+      <div>
+        <div>
+          {recipe ? (
+            <>
+              <div className="recipe">
+                <article>{response}</article>
+              </div>
+              <button className="recipe-button">I Cooked this Recipe!</button>
+              <button className="recipe-button" onClick={handleNewPrompt}>
+                Make a new recipe!{" "}
+              </button>
+            </>
           ) : (
             ""
-          )} */}
-					{/* </div> */}
-
-					<div>
-						{recipe ? (
-							<>
-								<div className='recipe'>
-									<article>{response}</article>
-								</div>
-								<button className='recipe-button'>I Cooked this Recipe!</button>
-								<button className='recipe-button' onClick={handleNewPrompt}>
-									Make a new recipe!{' '}
-								</button>
-							</>
-						) : (
-							''
-						)}
-					</div>
-				</div>
-			)}
-		</div>
-	);
-
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AddIngredients;
